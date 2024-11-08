990ec6ada6f770c4eedf21de8824b2a64164c559
BaronYashin <levyashin2504@gmail.com>
2024-11-08 22:02:09 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+HomeWork = 12
+print(HomeWork)
+Time = 1.5
+print(Time)
+NameCourse = "Python"
+print(NameCourse)
+TimeSpendOnOneTask = (Time / HomeWork)
+print(TimeSpendOnOneTask)
+print(TimeSpendOnOneTask, ' = среднее значение составляет от', Time, 'часов всего затраченного времени на', HomeWork, 'заданий')
